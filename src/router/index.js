523885453c47e48e13db671f9213e78ef884d3be
unknown <ersiajaral@gmail.com>
2025-04-08 00:23:04 +0800

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
+      path: '/',
       name: 'login',
       component: LoginView,
     },
