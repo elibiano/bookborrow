ca2aca79ff37b2541d9d63445fa5d1f406e046ee
unknown <ersiajaral@gmail.com>
2025-05-03 15:56:50 +0800

    feat: moved dashboard file; removed unnecessary

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import Dashboard from './views/auth/DashboardView.vue'
+import Dashboard from './views/system/DashboardView.vue'
 import Books from './views/auth/BooksView.vue'
 import Borrowings from './views/auth/BorrowingsView.vue'
 import Submission from './views/auth/SubmissionView.vue'
 import About from './views/auth/AboutView.vue'
 import Login from './views/auth/LoginView.vue'
-
+import Register from './views/auth/RegisterView.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
+    { path: '/register', component: Register },
     { path: '/dashboard', component: Dashboard },
     { path: '/books', component: Books },
     { path: '/borrowings', component: Borrowings },
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
-import AboutView from '@/views/auth/AboutView.vue'
-import BooksView from '@/views/auth/BooksView.vue'
-import BorrowingsView from '@/views/auth/BorrowingsView.vue'
-import ReturningView from '@/views/auth/ReturningView.vue'
-import DashboardView from '@/views/auth/DashboardView.vue'
+import DashboardView from '@/views/system/DashboardView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -20,29 +16,9 @@ const router = createRouter({
       component: RegisterView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
-    },
-    {
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
-    },
-    {
-      path: '/books',
-      name: 'books',
-      component: BooksView,
-    },
-    {
-      path: '/borrowings',
-      name: 'borrowings',
-      component: BorrowingsView,
-    },
-    {
-      path: '/returning',
-      name: 'returning',
-      component: ReturningView,
     },
   ],
 })
